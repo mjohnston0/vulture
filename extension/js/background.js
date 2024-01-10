62b042ad8c8a768f173dac421a2347fdd0f0f737
Charlie Oliver <2531385o@student.gla.ac.uk>
2024-01-10 14:39:11 +0000

    Changed sync to local background.js #39

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.local.get(['todoList'], function(result) {
         if (!result.todoList) {
-            let todoList = {};
+            let todoList = [];
             chrome.storage.local.set({todoList: todoList});
         }
     });
