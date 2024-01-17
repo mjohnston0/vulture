24ca6be4db8fd5b6a736b369e9b44f62b3905d85
Charlie Oliver <2531385o@student.gla.ac.uk>
2024-01-17 16:50:11 +0000

    Added page parsing and storage #43

--- extension/js/parser.js
+++ extension/js/parser.js
@@ -1,6 +1,36 @@
 
 // on load save entry
 window.onload = function() {
-    let text = document.body.innerText;
-    console.log(text);
+
+    let url = window.location.href;
+    let text = document.body.innerText.toLowerCase();
+
+    let keywords = text.split(/\s+/);
+
+    let keywordSet = new Set(keywords);
+
+    chrome.storage.local.get(['index'], function (result) {
+        console.log(result);
+        let newIndex = result;
+        for (let kw of keywordSet.keys()) {
+            kw = kw.replace(/[^a-z']/g, '');
+            kw = kw.replace(/^'|'$/g, '');
+            if (kw != '') {
+                if (kw in newIndex) {
+                    if (!url in newIndex[kw]) {
+                        newIndex[kw].push(url);
+                    }
+                } else {
+                    newIndex[kw] = [url];
+                }  
+            }
+        }
+
+        chrome.storage.local.set({result: newIndex});
+    })
+
+
+    chrome.storage.local.get(['index']), function (result) {
+        console.log(result.index);
+    }
 }
