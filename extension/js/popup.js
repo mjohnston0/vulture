ecc1a412b33154440e5768b5e41d714735c92035
2531385o@student.gla.ac.uk <2531385o@student.gla.ac.uk>
2024-01-31 22:15:13 +0000

    feat(popup): add more detailed task view on popup
    add ability to click tasks on the popup to get a more detailed view
    
    #63

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,3 +1,4 @@
+let taskView;
 window.addEventListener('click',function(e){
     if(e.target.href!==undefined && e.target.href.length > 0){
       chrome.tabs.create({url:e.target.href})
@@ -5,6 +6,7 @@ window.addEventListener('click',function(e){
   });
 
   document.addEventListener("DOMContentLoaded", function () {
+    taskView = document.querySelector(".task-view");
     chrome.storage.local.get(["todo"], function (result) {
         if (result.todo) {
             renderTable(result.todo.tasks);
@@ -13,12 +15,9 @@ window.addEventListener('click',function(e){
         }
     });
 
-    // updateTagBox();
-
   document.getElementById('task_filter').addEventListener('change', function() {
     drawTable();
   });
-
 });
 
 function drawTable() {
@@ -29,7 +28,9 @@ function drawTable() {
 
   function renderTable(tasksDict) {
     var table = document.getElementById("todo_table")
-    table.innerHTML = '<tr class="table-header"><th id="tbl-title">Title</th><th id="tbl-description">Description</th><th id="tbl-due">Due time</th><th id="tbl-tag">Tag</th><th id="tbl-status">Status</th</tr>'
+    table.innerHTML = '<thead><tr class="table-header"><th id="tbl-title">Title</th><th id="tbl-description">Description</th><th id="tbl-due">Due time</th><th id="tbl-tag">Tag</th><th id="tbl-status">Status</th</tr><thead>'
+    let tbody = document.createElement('tbody');
+    table.appendChild(tbody);
     let tasks = sortTasks(tasksDict);
 
     let filter_option = document.getElementById('task_filter').value;
@@ -38,7 +39,6 @@ function drawTable() {
     let toShow = []
 
     let targetDate = new Date();
-
     switch (filter_option) {
       case 'today':
         break;
@@ -65,7 +65,11 @@ function drawTable() {
       let taskID = entry[0];
       let task = entry[1];
 
-        var row = table.insertRow()
+        var row = tbody.insertRow()
+        row.addEventListener('click', function() {
+          console.log('click');
+          openTaskView(task);
+        })
         var titleCell = row.insertCell()
         titleCell.innerHTML = task.TITLE
 
@@ -117,4 +121,20 @@ function sortTasks(taskDict) {
   return entries;
 }
 
+function openTaskView(task) {
+  console.log(task);
+  document.getElementById('task-view-name').innerText = task.TITLE;
+  document.getElementById('task-view-description').innerText = task.DESCRIPTION;
+  document.getElementById('task-view-due').innerText = new Date(task.DUE).toLocaleString();
+  document.getElementById('task-view-tag').innerText = task.TAG;
 
+  taskView.style.display = 'flex';
+
+  document.getElementById('close-task').addEventListener('click', function() {
+    closeTaskView();
+  })
+}
+
+function closeTaskView() {
+  taskView.style.display = 'none';
+}
