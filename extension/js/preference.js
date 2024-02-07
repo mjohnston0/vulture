92418482ef2d97bd998ee9bb4ce73f1a8083775a
Jiusi Zhao <2541919Z@student.gla.ac.uk>
2024-02-07 13:21:36 +0000

    refactor(allow list): change option "SITE" to "DOMAIN", bug fix
    Change the dropdown to a label and change the word "SITE" to "DOMAIN"
    
    Fix a bug that toggle allow list item not working
    
    #73

--- extension/js/preference.js
+++ extension/js/preference.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function renderTable(allowlist) {
-  console.log(allowlist);
   var table = document.getElementById("allowlist_tbl");
   table.innerHTML =
     '<tr class="table-header"><th id="tbl-value">Value</th><th id="tbl-type">Type</th><th id="tbl-enable">Enable</th><th id="tbl-delete">Delete</th>';
@@ -19,12 +18,13 @@ function renderTable(allowlist) {
   Object.values(allowlist).forEach((element) => {
     var row = table.insertRow();
     var titleCell = row.insertCell();
+    var allowedItemID = element.ID;
     titleCell.innerHTML = element.VALUE;
 
     var descCell = row.insertCell();
     descCell.id = "data-des";
     let select = document.createElement("select");
-    let options = ["SITE", "PAGE", "REGEX"];
+    let options = ["DOMAIN", "PAGE", "REGEX"];
 
     options.forEach((element) => {
       select.options.add(new Option(element, element));
@@ -34,21 +34,37 @@ function renderTable(allowlist) {
 
     descCell.appendChild(select);
 
-    var radioCell = row.insertCell();
-    var radio = document.createElement("input");
-    radio.type = "radio";
-    radio.checked = element.IS_ACTIVE;
+    var checkCell = row.insertCell();
+    var checkActive = document.createElement("input");
+    
+    checkActive.type = "checkbox";
+    checkActive.checked = element.IS_ACTIVE;
+    
 
-    radioCell.appendChild(radio);
+    checkCell.appendChild(checkActive);
+
+    
+    checkActive.onchange = function (e) {
+      var check = this.checked;
+      chrome.storage.local.get(['allowlist'], function (result) {
+          let allowList = result.allowlist;
+          console.log(allowList.list)
+          allowList.list[allowedItemID].IS_ACTIVE = check;
+          chrome.storage.local.set({ allowlist: allowList });
+          renderTable(allowList.list);
+      })
+
+  }
+
 
     var deleteCell = row.insertCell();
     var deleteButton = document.createElement("button");
     deleteButton.textContent = "DELETE";
-    var deleteID = element.ID;
+
     deleteButton.addEventListener("click", function () {
       chrome.storage.local.get(["allowlist"], function (result) {
         let allowList = result.allowlist;
-        delete allowList.list[deleteID];
+        delete allowList.list[allowedItemID];
         allowList.count--;
         chrome.storage.local.set({ allowlist: allowList });
         renderTable(allowList.list);
@@ -69,7 +85,7 @@ document.getElementById("add_entry_btn").addEventListener("click", function () {
     list = result.allowlist.list;
     console.log(list);
     count = result.allowlist.count;
-    list[count] = { ID: count, VALUE: value, TYPE: type, IS_ACTIVE: true };
+    list[count] = { ID: count, IS_ACTIVE: true , TYPE: type,  VALUE: value};
     count++;
     chrome.storage.local.set({ allowlist: { count: count, list: list } });
     renderTable(list);
