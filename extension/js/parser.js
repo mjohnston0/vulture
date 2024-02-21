08a8ac9c760d859de23784e2eb225a0d02808efd
2914063k <2914063K@student.gla.ac.uk>
2024-02-21 12:34:17 +0000

    fix(parser): fix uncaught type error local storage for allowlist
    move isValid function to be inside parse_page function and separate parse and call it after the url is checked
    
    closed #89

--- extension/js/parser.js
+++ extension/js/parser.js
@@ -3,71 +3,60 @@ async function parse_page() {
     let url = window.location.href;
     let text = document.body.innerText.toLowerCase();
 
-    // console.log(url);
-
     let keywords = text.split(/\s+/);
 
     let keywordSet = new Set(keywords);
 
-    if (await isValid()) {
-        // console.log("TRUE");
+    chrome.storage.local.get(['allowlist'], function(result) {
+        let list = result.allowlist.list;
 
-        chrome.storage.local.get(['index'], function (result) {
-            let index = result.index;
-            for (let kw of keywordSet.keys()) {
-                kw = kw.replace(/[^a-z']/g, '');
-                kw = kw.replace(/^'|'$/g, '');
-                if (kw != '') {
-                    if (index[kw]) {
-                        if (!index[kw].includes(url)) {
-                            index[kw].push(url);
-                        } else {
-                            index[kw].push(index[kw].splice(index[kw].indexOf(url), 1)[0]);
-                        }
-                    } else {
-                        index[kw] = [url];
+        let domain = window.location.hostname;
+    
+        for (element of Object.values(list)) {
+    
+            console.log(element);
+    
+            if (element.IS_ACTIVE) {
+    
+                if (element.TYPE === 'DOMAIN' && element.VALUE.localeCompare(domain) === 0) parse(keywordSet, url);
+    
+                if (element.TYPE === 'PAGE' && element.VALUE.localeCompare(url) === 0) parse(keywordSet, url);
+    
+                if (element.TYPE === 'REGEX'){
+                    let reg = new RegExp(element.VALUE);
+                    if(reg.exec(url) != null){
+                        
+                        parse(keywordSet, url)
                     }
                 }
             }
+        }
+    });
 
-            // console.log(index);
-            chrome.storage.local.set({ "index": index });
-        })
-    }
-    else {
-        // console.log("FALSE");
-    }
 }
 
-async function isValid() {
-    const result = await chrome.storage.local.get(['allowlist']);
-
-    let list = result.allowlist.list;
-
-    let domain = window.location.hostname;
-    let url = window.location.href;
-
-    for (element of Object.values(list)) {
-
-        // console.log(element);
-
-        if (element.IS_ACTIVE) {
-
-            if (element.TYPE === 'DOMAIN' && element.VALUE.localeCompare(domain) === 0) return true;
-
-            if (element.TYPE === 'PAGE' && element.VALUE.localeCompare(url) === 0) return true;
-
-            if (element.TYPE === 'REGEX'){
-                let reg = new RegExp(element.VALUE);
-                if(reg.exec(url) != null){
-                    return true;
+function parse(keywordSet, url) {
+    chrome.storage.local.get(['index'], function (result) {
+        let index = result.index;
+        for (let kw of keywordSet.keys()) {
+            kw = kw.replace(/[^a-z']/g, '');
+            kw = kw.replace(/^'|'$/g, '');
+            if (kw != '') {
+                if (index[kw]) {
+                    if (!index[kw].includes(url)) {
+                        index[kw].push(url);
+                    } else {
+                        index[kw].push(index[kw].splice(index[kw].indexOf(url), 1)[0]);
+                    }
+                } else {
+                    index[kw] = [url];
                 }
             }
         }
-    }
 
-    return false
-
+        console.log(index);
+        chrome.storage.local.set({ "index": index });
+    })
 }
 
 navigation.addEventListener("navigate", parse_page());
